f83d42e299b066695ffe094fc443f966336a5b6c
Kilian Calefice <kilianc0709@gmail.com>
2025-11-21 15:57:09 +0100

    refa: removed unused resources

--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "tunnel_token" {
-  description = "Token for the client to authorize to the tunnel"
-  value       = cloudflare_zero_trust_tunnel_cloudflared.main.tunnel_token
-  sensitive   = true
-}
